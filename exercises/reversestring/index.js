586f76de0932496bc16c71fd2345595afa755d40
Dasol Park <dasol.park615@gmail.com>
2019-10-30 13:36:37 +0900

    Test VS Code Node.js Debugger

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -7,11 +7,8 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-  debugger;
   return str.split('').reduce((rev, char) => char + rev, '');
 }
-
-reverse('abhasdg');
 
 module.exports = reverse;
 
