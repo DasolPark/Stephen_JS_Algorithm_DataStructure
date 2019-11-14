109a2c480354a7d70f92ae2960a2d51e210fcdd1
Dasol Park <dasol.park615@gmail.com>
2019-11-14 18:05:07 +0900

    74. Solve for Size

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -13,8 +13,21 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+
   insertFirst(data) {
     this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+
+    return counter;
   }
 }
 
