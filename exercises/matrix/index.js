42214d8f4cc7a951181b1412fc2c37cb7120238d
Dasol Park <dasol.park615@gmail.com>
2020-01-01 13:19:03 +0900

    More on Spiral Matrix for visual

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -61,3 +61,57 @@ function matrix(n) {
 }
 
 module.exports = matrix;
+
+// For Visual
+
+// function matrix(n) {
+//   const results = [];
+
+//   for (let i = 0; i < n; i++) {
+//     results.push([]);
+//   }
+
+//   let counter = 1;
+//   let startColumn = 0;
+//   let endColumn = n - 1;
+//   let startRow = 0;
+//   let endRow = n - 1;
+//   while (startColumn <= endColumn && startRow <= endRow) {
+//     // Top Row
+//     for (let i = startColumn; i <= endColumn; i++) {
+//       results[startRow][i] = counter;
+//       console.log('1st', counter);
+//       counter++;
+//     }
+//     startRow++;
+
+//     // Right Column
+//     for (let i = startRow; i <= endRow; i++) {
+//       results[i][endColumn] = counter;
+//       console.log('2nd', counter);
+//       counter++;
+//     }
+//     endColumn--;
+
+//     // Bottom Row
+//     for (let i = endColumn; i >= startColumn; i--) {
+//       results[endRow][i] = counter;
+//       console.log('3rd', counter);
+//       counter++;
+//     }
+//     endRow--;
+
+//     // Start Column
+//     for (let i = endRow; i >= startRow; i--) {
+//       results[i][startColumn] = counter;
+//       console.log('4th', counter);
+//       counter++;
+//     }
+//     startColumn++;
+//   }
+
+//   return results;
+// }
+
+// console.log(matrix(3));
+// console.log(matrix(4));
